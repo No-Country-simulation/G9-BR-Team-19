18d2b23fef6fa93f7bd530486554582ffe618f9b
Leo <leokhayim@gmail.com>
2026-07-07 21:27:40 -0300

    feat: adicionar projeto Spring na subpasta \backend

---
+++ backend/team19/src/main/java/com/techmind/team19/Team19Application.java
@@ -0,0 +1,13 @@
+package com.techmind.team19;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team19Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team19Application.class, args);
+	}
+
+}
---
+++ backend/team19/src/test/java/com/techmind/team19/Team19ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techmind.team19;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team19ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
