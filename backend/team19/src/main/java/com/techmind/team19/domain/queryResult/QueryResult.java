0e1519676a29c2e33bda80364bbb8daccb108daf
Rafael Arcanjo <rafael.arcanjo.dssilva@gmail.com>
2026-07-31 23:59:34 -0300

    feat: Criar tabela query_result e tag com a tabela de Relacionamento query_result_tags

---
+++ backend/team19/src/main/java/com/techmind/team19/domain/queryResult/QueryResult.java
@@ -0,0 +1,51 @@
+package com.techmind.team19.domain.queryResult;
+
+import com.techmind.team19.domain.tag.Tag;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+
+@Table(name = "query_result")
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class QueryResult {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String Text;
+
+    private String Category;
+
+    private Double probability;
+
+    @ManyToMany
+    @JoinTable(
+            name = "query_result_tags",
+            joinColumns = @JoinColumn(name = "query_result_id"),
+            inverseJoinColumns = @JoinColumn(name = "tag_id")
+    )
+    private Set<Tag> tags = new HashSet<>();
+
+    @Column(name = "created_at", updatable = false)
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
---
+++ backend/team19/src/main/java/com/techmind/team19/domain/tag/TagRepository.java
@@ -0,0 +1,6 @@
+package com.techmind.team19.domain.tag;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TagRepository extends JpaRepository<Tag, Long> {
+}
