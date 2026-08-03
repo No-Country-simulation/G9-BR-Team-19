314814702bbfe831cdfaad5faa240b1ba536c1c9
Italo <italof363@gmail.com>
2026-08-03 20:48:58 -0300

    Adicionado consulta de respostas

--- Mobile/techmind/lib/repository/consulta_conteudo_repository.dart
+++ Mobile/techmind/lib/repository/consulta_conteudo_repository.dart
@@ -1,17 +1,21 @@
 import 'package:techmind/models/consulta_conteudo.dart';
+import 'package:techmind/models/consulta_resposta.dart';
 import 'package:techmind/service/api_service.dart';
 
 class ConsultaConteudoRepository {
   ApiService api = ApiService();
 
-  Future<ConsultaConteudo> enviarDadosConsulta(
-    ConsultaConteudo consultaConteudo,
-  ) async {
-    final response = await api.post(
-      "/conteudos/processar",
-      consultaConteudo.toJson(),
-    );
-
-    return ConsultaConteudo.fromJson(response);
+  Future<Object> enviarDadosConsulta(ConsultaConteudo consultaConteudo) async {
+    try {
+      final response = await api.post(
+        "/conteudos/processar",
+        consultaConteudo.toJson(),
+      );
+      print(response);
+      return ConsultaResposta.fromjson(response);
+    } catch (e) {
+      print(e);
+    }
+    return 'Erro ao fazer requisição';
   }
 }
