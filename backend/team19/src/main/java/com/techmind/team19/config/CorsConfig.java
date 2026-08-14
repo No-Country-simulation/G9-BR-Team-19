678b72870e863a1d9bc7e51d2c951834d61622ac
Leo <leokhayim@gmail.com>
2026-08-13 23:46:19 -0300

    usa window location hostname para url da VM

--- backend/team19/src/main/java/com/techmind/team19/config/CorsConfig.java
+++ backend/team19/src/main/java/com/techmind/team19/config/CorsConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
-    @Value("${app.cors.allowed-origins:*}")
+    @Value("${CORS_ALLOWED_ORIGINS:*}")
     private String[] allowedOrigins;
 
     @Override
